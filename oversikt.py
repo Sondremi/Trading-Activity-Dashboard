a46c97132e40a280875c8d72995a9c43136191d2
Sondre Indset <sondre2004@hotmail.no>
2023-10-22 19:06:25 +0200

    Update oversikt.py

--- oversikt.py
+++ oversikt.py
@@ -8,17 +8,16 @@ def hovedprogram(inp):
 
     Args:
         inp (int): User input to select the type of analysis to perform.
-            0: Dynamic analysis
-            1: Static and monthly analysis
-            2: Total transaction summary
+            1: Dynamic analysis
+            2: Static and monthly analysis
+            3: Total transaction summary
 
     Returns:
         None
     """
     prog = Analyse()
-    # inp = 1
 
-    if inp == 0:
+    if inp == 1:
         # Dynamic stats
         # Stats like this week and this month
         print("-"*40)
@@ -28,10 +27,10 @@ def hovedprogram(inp):
         print("P/L denne uken: ", prog.denne_uken(), "kr")
         print("P/L denne mnd: ", prog.denne_mnd(), "kr")
         print("P/L i år: ", prog.dette_året(), "kr")
-        print("P/L totalt: ", prog.sum_totalt())
+        print("P/L totalt: ", prog.sum_totalt(), "kr")
         print("-"*40)
         
-    elif inp == 1:
+    elif inp == 2:
         # Static stats
         # Stats like 1 week back, or 1 month back
         print("-"*40)
@@ -42,20 +41,49 @@ def hovedprogram(inp):
         print("P/L 1 mnd: ", prog.sum_mnd_tilbake(1), "kr")
         print("P/L 3 mnd: ", prog.sum_mnd_tilbake(3), "kr")
         print("P/L 6 mnd: ", prog.sum_mnd_tilbake(6), "kr")
-        print("P/L totalt: ", prog.sum_totalt())
+        print("P/L totalt: ", prog.sum_totalt(), "kr")
         print("-"*40)
 
-    elif inp == 2:
+    elif inp == 3:
         # Extra stats
         # Total trades, win and loss
         # And P/L ratio in %
         print("-"*40)
-        print("Balance: ", prog.balance())
+        print(prog)
+        print("Balance: ", prog.balance(), "kr")
         print("Trades totalt: ", prog.total_trades())
         print("Price totalt: ", round(sum(prog._tot_dep), 2), "kr")
         print("Loss totalt: ", round(sum(prog._tot_wit), 2), "kr")
         print("P/L % ratio: ", prog.ratio(), "%")
         # Keep in mind that the P/L ratio will be affected by overnight fees
         print("-"*40)
+    
 
-hovedprogram(2)
+def kjor_hovedprogram():
+    """
+    Secondary main program for running the original program.
+    
+    Calls main program with args selected by user
+    
+    Args:
+        None
+    
+    Returns:
+        None
+    """
+    print("Velg et alternativ: ")
+    print("1. Dynamic stats")
+    print("2. Static stats")
+    print("3. Extra stats")
+    print("4. All stats")
+    inp = int(input("Valg: "))
+
+    assert inp in [1, 2, 3, 4], "Velg alternativ (1-4)"
+    
+    if inp != 4:
+        hovedprogram(inp)
+    else:
+        for i in range(4):
+            hovedprogram(i)
+
+kjor_hovedprogram()
